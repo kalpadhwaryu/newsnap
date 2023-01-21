f1439220829f210b250d1c36e025c30d94ad058c
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2023-01-21 12:27:08 +0530

    Added Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import React, {FunctionComponent, type PropsWithChildren} from 'react';
+import React, {
+  FunctionComponent,
+  useEffect,
+  type PropsWithChildren,
+} from 'react';
 import {View} from 'react-native';
 import Explore from './components/Explore';
 import Header from './components/Header';
 import Main from './components/Main';
 import tw from 'tailwind-react-native-classnames';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: FunctionComponent = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View style={[tw`flex-1 bg-white w-full`]}>
       <Header />
